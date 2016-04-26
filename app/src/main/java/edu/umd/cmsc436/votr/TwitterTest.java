8d178f45d5a62de0b4c019cb24b890ddf00948d4
Eric Huang <tkhuang@terpmail.umd.edu>
2016-04-26 14:43:12 -0400

    t

---
+++ app/src/main/java/edu/umd/cmsc436/votr/TwitterTest.java
@@ -0,0 +1,44 @@
+package edu.umd.cmsc436.votr;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.net.URL;
+
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+
+/**s
+ * Created by kk on 4/26/16.
+ */
+public class TwitterTest extends AsyncTask<String, Void, String> {
+
+    protected String doInBackground(String... tags) {
+
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+
+            twitter.setOAuthConsumer("RK0ePlZwYZalKH9ZZVFjIVJVR",
+                    "xWihEroKYZyRUaCFYhaumTL6cM8F7R6yilSEK5dFP6X9LnlGMQ");
+
+            twitter.setOAuthAccessToken(new AccessToken(
+                    "3711878716-0NSO7UxyiXkLfFPdexICDSBQRVWTSsSj1RYg6vA",
+                    "x88A8QvV4jXXvatUmnWIYqa6h75pfr1wobVxcPewxSFh4"));
+
+            Queryit query = new Query(tags[0]);
+            QueryResult result = twitter.search(query);
+
+            for (twitter4j.Status status : result.getTweets()) {
+                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    protected void onProgressUpdate(String result) {
+    }
+}
