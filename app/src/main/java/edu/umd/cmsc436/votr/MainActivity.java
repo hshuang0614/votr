887e25af4e998d72f48fd7ee8370b1c244ce6eff
Fahed Hijazi <fahed.dev@gmail.com>
2016-04-28 13:03:41 -0400

    Fix build and add buttons for polling loc

--- app/src/main/java/edu/umd/cmsc436/votr/MainActivity.java
+++ app/src/main/java/edu/umd/cmsc436/votr/MainActivity.java
@@ -1,19 +1,30 @@
 package edu.umd.cmsc436.votr;
 
 import android.content.Intent;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button candidatesButton = (Button) findViewById(R.id.candidates);
 
+        setContentView(R.layout.activity_main);
+
+        // Initialize LocationManager.
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // Initialize buttons
+        Button candidatesButton = (Button) findViewById(R.id.candidates);
+        Button pollingLocationButton = (Button) findViewById(R.id.polling_location);
+        Button earlyVoteSites = (Button) findViewById(R.id.early_vote_sites);
+
+        // Setup listeners
         candidatesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,8 +33,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        pollingLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
-
-
+        earlyVoteSites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
